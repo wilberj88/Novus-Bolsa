af51d450be483460700eb343534b349480691f39
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2024-01-03 04:41:32 +0100

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -3,6 +3,8 @@ import requests
 from streamlit_echarts import st_echarts
 import pandas as pd
 from lightweight_charts.widgets import StreamlitChart
+import random
+import pandas as pd
 
 if "symbols_list" not in st.session_state:
     st.session_state.symbols_list = None
@@ -135,4 +137,28 @@ with params_col:
                     render_basic_radar()
                     
             with data_col:
-                    st.dataframe(hist_df, height = 470)
+                df = pd.DataFrame(
+                    {
+                        "name": ["Roadmap", "Extras", "Issues"],
+                        "url": ["https://roadmap.streamlit.app", "https://extras.streamlit.app", "https://issues.streamlit.app"],
+                        "stars": [random.randint(0, 1000) for _ in range(3)],
+                        "views_history": [[random.randint(0, 5000) for _ in range(30)] for _ in range(3)],
+                    }
+                )
+                st.dataframe(
+                    df,
+                    column_config={
+                        "name": "App name",
+                        "stars": st.column_config.NumberColumn(
+                            "Github Stars",
+                            help="Number of stars on GitHub",
+                            format="%d ⭐",
+                        ),
+                        "url": st.column_config.LinkColumn("App URL"),
+                        "views_history": st.column_config.LineChartColumn(
+                            "Views (past 30 days)", y_min=0, y_max=5000
+                        ),
+                    },
+                    hide_index=True,
+                )
+                                  

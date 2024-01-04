4bb6b797ccbe56d7eeb09191dc594e24e1533ad8
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2024-01-04 21:05:59 +0100

    Update tempo.py

--- pages/tempo.py
+++ pages/tempo.py
@@ -25,6 +25,17 @@ else:
         ts.sleep(per)  
 
 
+st.title('1 second Timer')
+hora = "00:01:00"
+sec = converter(str(hora))
+bar = st.progress(0)
+per = sec/100
+progress_status = st.empty()
+for i in range(100):
+    bar.progress((i+1))
+    progress_status.write(str(i+1)+" %")
+    ts.sleep(per) 
+
 st.title('1 minute Timer')
 hora = "01:00:00"
 sec = converter(str(hora))
@@ -48,13 +59,4 @@ for i in range(100):
     progress_status.write(str(i+1)+" %")
     ts.sleep(per)  
 
-st.title('1 second Timer')
-hora = "00:01:00"
-sec = converter(str(hora))
-bar = st.progress(0)
-per = sec/100
-progress_status = st.empty()
-for i in range(100):
-    bar.progress((i+1))
-    progress_status.write(str(i+1)+" %")
-    ts.sleep(per)  
+ 

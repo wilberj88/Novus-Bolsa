b0279d78f3df807c76d13d282082400f4dd47524
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2024-01-04 19:53:23 +0100

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -104,18 +104,6 @@ with params_col:
             with chart_col:
 
                 with st.container(border=True):        
-                    current_time = time.localtime()
-                    seconds_left = 59 - current_time.tm_sec
-                    minutes_left = 59 - current_time.tm_min
-                    hours_left = 23 - current_time.tm_hour
-                    total_seconds = seconds + minutes * 60 + hours * 3600 + days * 86400
-
-                    bar = st.progress(0)
-                    for i in range(total_seconds):
-                        bar.progress((i+1)*10)
-                        time.sleep(1)             
-
-                    
                     def render_basic_radar():
                         option = {
                                 "title": {"text": "Costos estimados por tipos de Riesgos Climáticos"},

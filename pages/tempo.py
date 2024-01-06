58fbfa52f39749c872ef17ccb675ddf0e8ad7479
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2024-01-06 17:10:32 +0100

    Update tempo.py

--- pages/tempo.py
+++ pages/tempo.py
@@ -2,6 +2,21 @@ import streamlit as st
 import time as ts
 import pandas as pd
 from datetime import time
+from d3blocks import D3Blocks
+
+
+# Initialize
+d3 = D3Blocks()
+
+
+# Load example data
+df = d3.import_example('random_time', n=10000, c=300, date_start="1-1-2000 00:10:05", date_stop="1-1-2000 23:59:59")
+
+
+# Plot
+d3.movingbubbles(df, speed={"slow": 1000, "medium": 200, "fast": 10}, filepath='movingbubbles.html')
+
+
 
 
 st.title('General Timer')

893683a42d4424dc820643227a6ebc044c248d00
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2024-01-06 17:48:43 +0100

    Update tempo.py

--- pages/tempo.py
+++ pages/tempo.py
@@ -4,7 +4,40 @@ import pandas as pd
 from datetime import time
 import time
 from streamlit_lottie import st_lottie
+from streamlit.components.v1 import html
 
+my_html = """
+<script>
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10)
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+
+window.onload = function () {
+    var fiveMinutes = 60 * 5,
+        display = document.querySelector('#time');
+    startTimer(fiveMinutes, display);
+};
+</script>
+
+<body>
+  <div>Registration closes in <span id="time">05:00</span> minutes!</div>
+</body>
+"""
+
+html(my_html)
 
 st.title('General Timer')
 def converter(value):
